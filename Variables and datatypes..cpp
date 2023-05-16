237edac4b49d17732f60b429ffbc7bebebbee16a
odawaNg4l4 <odawangala99@gmail.com>
2023-05-16 15:13:03 +0300

    c++

---
+++ Variables and datatypes..cpp
@@ -0,0 +1,72 @@
+//Integers
+#include <iostream>
+using namespace std;
+
+int myNum = 25;
+int main()
+{
+    cout << myNum << endl;
+    return 0;
+}
+
+//Float
+#include<iostream>
+using namespace std;
+
+int myFloatNum = 23.45;
+int main()
+{
+    cout <<myFloatNum<<endl;
+    return 0;
+}
+
+//Strings
+#include<iostream>
+using namespace std;
+
+string myStatement = "I am a woman";
+int main()
+{
+    cout << myStatement<<endl;
+    return 0;
+}
+
+//Displaying variables
+#include <iostream>
+using namespace std;
+int myAge = 35;
+int main()
+{
+    cout <<"I am" << myAge << "yrs old";
+}
+
+//Many Variables
+#include<iostream>
+using namespace std;
+int x=5, y=7, z=12;
+int main()
+{
+    cout << x+y+z ;
+}
+
+//One value to multiple variables
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y, z;
+  x = y = z = 50;
+  cout << x + y + z;
+  return 0;
+}
+
+//CONSTANT
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    const float myFloat = 3658849.9998;
+    cout << myFloat;
+    return 0;
+}
