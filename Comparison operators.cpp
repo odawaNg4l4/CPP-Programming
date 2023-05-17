90e4cb2b27629947e75b02670f6ca27836e21844
odawaNg4l4 <odawangala99@gmail.com>
2023-05-17 20:41:51 +0300

    c++

---
+++ Comparison operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 7;
+    int y = 12;
+    cout << (x >y) << endl;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 142;
+    int y = 14322;
+    cout <<(x < y);
+    return 0;
+}
