97826b59e96db48ab3bd8157bcfb259c80b9c3aa
odawaNg4l4 <odawangala99@gmail.com>
2023-05-17 19:59:52 +0300

    c++

---
+++ User input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout << "Type 1 number" << endl;
+    cout << "Type 2 number" << endl;
+    cin>> x;
+    cin>>y;
+    cout<<"Your sum is\t" << x + y  ;
+    return 0;
+}
