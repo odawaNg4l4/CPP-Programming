7ff2fca6c24950774bff52b4988c3a59a6e722f5
odawaNg4l4 <odawangala99@gmail.com>
2023-05-25 20:35:20 +0300

    c++

--- Boolean and Conditions.cpp
+++ Boolean and Conditions.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main()
@@ -29,6 +29,45 @@ int main()
     int votingAge - 18;
     cout << myAge >= votingAge;
     return 0;
+}
+
+//PLUS IF
+#include<iostream>
+using namespace std;
+int main()
+{
+    int myAge = 25;
+    int votingAge = 18;
+
+    if(myAge>=votingAge)
+    {
+        cout<< "Old enough to vote";
+    }
+    else
+    {
+        cout<<"Not old enough to vote;
+    }
+}*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 50;
+    int y = 50;
+    if(x==y)
+    {
+        cout<< "1";
+    }
+    else if(x>y)
+    {
+        cout<< "2";
+    }
+    else()
+    {
+        cout<< "3";
+    }
 }
 
 
