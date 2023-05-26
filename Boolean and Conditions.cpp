ae3b604fe7bc9337b5df38895f8ac0f4bb1da5ae
odawaNg4l4 <odawangala99@gmail.com>
2023-05-26 08:59:53 +0300

    c++

--- Boolean and Conditions.cpp
+++ Boolean and Conditions.cpp
@@ -70,14 +70,15 @@ int main()
     }
 }
 
+//Short hand if else
 #include<iostream>
+
 using namespace std;
 
 int main()
 {
-int time = 20;
-    string result = (time < 18) ? "Good day." : "Good evening.";
-    cout << result;
+    int age = 21;
+    string result =(age>21)? "Adult" : "Child";
 }
 
 
