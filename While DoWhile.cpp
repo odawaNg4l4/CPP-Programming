28e3c516fb949497b20f3b285726b00efb55b969
odawaNg4l4 <odawangala99@gmail.com>
2023-06-02 17:19:13 +0300

    c++

--- While DoWhile.cpp
+++ While DoWhile.cpp
@@ -56,4 +56,15 @@ int main()
     }
 }
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for (int i : myNumbers) {
+    cout << i << "\n";
+    }
+}
+
 
