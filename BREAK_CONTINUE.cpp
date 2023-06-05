e1c3651d66cbe6fa510efb72773f4776fbc8e08c
odawaNg4l4 <odawangala99@gmail.com>
2023-06-05 15:49:47 +0300

    c++

--- BREAK_CONTINUE.cpp
+++ BREAK_CONTINUE.cpp
@@ -11,3 +11,18 @@ for (int i = 0; i < 10; i++) {
   cout << i << "\n";
 }
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0;
+    while(i < 10)
+    {
+        cout << i << "\n";
+        i++;
+        if (i == 9){
+            break;
+        }
+    }
+}
