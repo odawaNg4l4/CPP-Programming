942f5e2b07c9694d026350c612c543de79c93e9d
odawaNg4l4 <odawangala99@gmail.com>
2023-06-07 20:17:05 +0300

    c++

--- ARRAYS.cpp
+++ ARRAYS.cpp
@@ -8,3 +8,24 @@ int main()
     cout << dogs[0];
     return 0;
 }
+
+//CHANGING ARRAY ELEMENTS
+#include<iostream>
+using namespace std;
+int main()
+{
+    string dogs[5] = {"bulldog", "retriever","husky","poodle", "chihuahua"};
+    dogs[0] = "Boerboel";
+    cout << dogs[0];
+}
+
+//ARRAYS AND LOOPS
+#include<iostream>
+using namespace std;
+int main()
+{
+   string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+for (int i = 0; i < 5; i++) {
+  cout << cars[i] << "\n";
+}
+}
