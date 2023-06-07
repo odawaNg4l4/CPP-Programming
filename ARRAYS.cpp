80ef279db8d1cb17e3fe1c04a0ab1163b6fa2efc
odawaNg4l4 <odawangala99@gmail.com>
2023-06-07 20:19:37 +0300

    c++

--- ARRAYS.cpp
+++ ARRAYS.cpp
@@ -29,3 +29,23 @@ for (int i = 0; i < 5; i++) {
   cout << cars[i] << "\n";
 }
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+for (int i = 0; i < 5; i++) {
+  cout << i << " = " << cars[i] << "\n";
+}
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+for (int i = 0; i < 5; i++) {
+  cout << myNumbers[i] << "\n";
+}
+}
