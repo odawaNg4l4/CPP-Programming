bd9ea299457c750d631d379caa96c43cd03e7e06
odawaNg4l4 <odawangala99@gmail.com>
2023-06-07 20:28:30 +0300

    c++

--- ARRAYS.cpp
+++ ARRAYS.cpp
@@ -19,33 +19,12 @@ int main()
     cout << dogs[0];
 }
 
-//ARRAYS AND LOOPS
 #include<iostream>
 using namespace std;
 int main()
 {
-   string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
-for (int i = 0; i < 5; i++) {
-  cout << cars[i] << "\n";
-}
-}
-
-#include<iostream>
-using namespace std;
-int main()
-{
-  string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
-for (int i = 0; i < 5; i++) {
-  cout << i << " = " << cars[i] << "\n";
-}
-}
-
-#include<iostream>
-using namespace std;
-int main()
-{
-  int myNumbers[5] = {10, 20, 30, 40, 50};
-for (int i = 0; i < 5; i++) {
-  cout << myNumbers[i] << "\n";
+    string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+    for (int i = 0; i < 5; i++) {
+    cout << cars[i] << "\n";
 }
 }
