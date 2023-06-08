900c094ff64ee3568513ec8fecc3d961bcbce813
odawaNg4l4 <odawangala99@gmail.com>
2023-06-08 07:29:00 +0300

    c++

--- ARRAYS.cpp
+++ ARRAYS.cpp
@@ -29,3 +29,14 @@ int main()
         cout << dogs[i] <<"\n";
     }
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string dogs[5] = {"bulldog", "retriever","husky","poodle", "chihuahua"};
+    for(int i = 0; i < 5; i++)
+    {
+        cout<<i<<"="<<dogs[i]<<"\n";
+    }
+}
