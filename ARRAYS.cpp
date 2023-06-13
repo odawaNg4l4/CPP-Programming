c662eccadb6ed0104c60e88c861ee2ebd8287e9c
odawaNg4l4 <odawangala99@gmail.com>
2023-06-13 21:05:13 +0300

    c++

--- ARRAYS.cpp
+++ ARRAYS.cpp
@@ -62,16 +62,16 @@ int main()
     cout << getArrayLength;
 }
 
-
+//Loop through an array
 #include<iostream>
 using namespace std;
 
 int main()
 {
     int myNumbers[5] = {10, 20, 30, 40, 50};
-    for (int i = 0; i < 5; i++) {
-    cout << myNumbers[i] << "\n";
-}
+    for (int i = 0; i < sizeof(myNumbers) / sizeof(int); i++){
+        cout << myNumbers[i] << "\n";
+    }
 }
 
 
