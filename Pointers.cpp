8b66d9202ec1469714dd3a0e23fe41b8f3ca5a40
odawaNg4l4 <odawangala99@gmail.com>
2023-06-21 11:00:52 +0300

    c++

--- Pointers.cpp
+++ Pointers.cpp
@@ -8,3 +8,27 @@ int main()
     cout << &food << "\n";
     cout << ptr << "\n";
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string food = "Ugali";
+    string* ptr = &food;
+    cout << ptr << "\n";
+    cout << *ptr << "\n";
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string food ="Ugali";
+    string *ptr = &food;
+    cout << food << "\n";
+    cout << &food << "\n";
+    cout << *ptr << "\n";
+    *ptr = "Hamburger";
+    cout << *ptr << "\n";
+    cout << food << "\n";
+}
