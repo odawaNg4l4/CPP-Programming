25986542feccab534eed35997c29dba6291a21da
odawaNg4l4 <odawangala99@gmail.com>
2023-06-22 22:46:53 +0300

    c++

--- Functions.cpp
+++ Functions.cpp
@@ -23,7 +23,7 @@ int main() {
   myFunction("Jenny");
   myFunction("Anja");
   return 0;
-}*/
+}
 
 #include<iostream>
 using namespace std;
@@ -36,5 +36,19 @@ int main() {
   myFunction("India");
   myFunction();
   myFunction("USA");
+  return 0;
+}*/
+
+
+#include<iostream>
+using namespace std;
+void myFunction(string fname, int age) {
+  cout << fname << " Refsnes. " << age << " years old. \n";
+}
+
+int main() {
+  myFunction("Liam", 3);
+  myFunction("Jenny", 14);
+  myFunction("Anja", 30);
   return 0;
 }
