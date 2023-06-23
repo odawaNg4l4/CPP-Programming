599cbf3aa39bbf6c753efcfa24f22392eb798c64
odawaNg4l4 <odawangala99@gmail.com>
2023-06-23 19:23:09 +0300

    c++

--- Functions.cpp
+++ Functions.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 using namespace std;
 
 void myFunction();
@@ -62,7 +62,7 @@ int myFunction(int x) {
 int main() {
   cout << myFunction(3);
   return 0;
-}*/
+}
 
 #include<iostream>
 using namespace std;
