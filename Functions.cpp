ff1686f7b1e28bb42722ee9e2b0519998f7c09a5
odawaNg4l4 <odawangala99@gmail.com>
2023-06-27 21:22:40 +0300

    c++

--- Functions.cpp
+++ Functions.cpp
@@ -149,3 +149,20 @@ int main() {
   cout << "Double: " << myNum2;
   return 0;
 }
+
+//RECURSION
+#include<iostream>
+using namespace std;
+int sum(int k) {
+  if (k > 0) {
+    return k + sum(k - 1);
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  int result = sum(10);
+  cout << result;
+  return 0;
+}
