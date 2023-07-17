b5141f49d45ee215d7271389c57ae0b53cc8b752
odawaNg4l4 <odawangala99@gmail.com>
2023-07-17 13:23:06 +0300

    c++

--- Classes.cpp
+++ Classes.cpp
@@ -1,3 +1,4 @@
+/*Create an object
 #include<iostream>
 using namespace std;
 
@@ -17,7 +18,32 @@ class MyClass
     cout << myObj.myString;
 
     return 0;
+}*/
+
+//MULTIPLE OBJECST
+#include<iostream>
+using namespace std;
+
+class Car{
+    public:
+    string brand;
+    string model;
+    int year;
+};
+
+int main(){
+    Car carObj1;
+    carObj1.brand = "BMW";
+    carObj1.model = "X5";
+    carObj1.year = 1999;
+
+    Car carObj2;
+    carObj2.brand = "Mercedes";
+    carObj2.model = "C200";
+    carObj2.year = 2006;
+
+    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year<< " " << "\n";
+    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << " " << "\n";
+    return 0;
 }
-
-
 
