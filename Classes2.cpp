3eeaf768addf4c0259066661937869e519f81fb7
odawaNg4l4 <odawangala99@gmail.com>
2023-07-19 21:29:37 +0300

    c++

--- Classes2.cpp
+++ Classes2.cpp
@@ -86,3 +86,23 @@ int main(){
     cout << myCar.brand + " " + myCar.model;
     return 0;
 }
+
+//MULTILEVEL INHERITANCE
+class MyClass {
+  public:
+    void myFunction() {
+      cout << "Some content in parent class." ;
+    }
+};
+
+class MyChild: public MyClass {
+};
+
+class MyGrandChild: public MyChild {
+};
+
+int main() {
+  MyGrandChild myObj;
+  myObj.myFunction();
+  return 0;
+}
