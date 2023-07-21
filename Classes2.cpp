b1a20b4472ad54b4a871c8c26622101dadd15275
odawaNg4l4 <odawangala99@gmail.com>
2023-07-21 22:35:05 +0300

    c++

--- Classes2.cpp
+++ Classes2.cpp
@@ -162,3 +162,26 @@ int main() {
   cout << "Bonus: " << myObj.bonus << "\n";
   return 0;
 }
+
+class Animal {
+  public:
+    void animalSound() {
+      cout << "The animal makes a sound \n";
+    }
+};
+
+// Derived class
+class Pig : public Animal {
+  public:
+    void animalSound() {
+      cout << "The pig says: wee wee \n";
+    }
+};
+
+// Derived class
+class Dog : public Animal {
+  public:
+    void animalSound() {
+      cout << "The dog says: bow wow \n";
+    }
+};
