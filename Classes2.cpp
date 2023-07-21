7151b65bf6bf2d5650ecfe1218c3f4fd002b4729
odawaNg4l4 <odawangala99@gmail.com>
2023-07-21 22:37:41 +0300

    c++

--- Classes2.cpp
+++ Classes2.cpp
@@ -1,4 +1,4 @@
-//Constructor Parameters
+/*Constructor Parameters
 #include<iostream>
 using namespace std;
 
@@ -185,3 +185,19 @@ class Dog : public Animal {
       cout << "The dog says: bow wow \n";
     }
 };
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  // Create and open a text file
+  ofstream MyFile("filename.txt");
+
+  // Write to the file
+  MyFile << "Files can be tricky, but it is fun enough!";
+
+  // Close the file
+  MyFile.close();
+}
