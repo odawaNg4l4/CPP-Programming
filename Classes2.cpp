e460e4d7dfd1fcaeef7d26f5506eb5831a5a9794
odawaNg4l4 <odawangala99@gmail.com>
2023-07-24 21:40:27 +0300

    c++

--- Classes2.cpp
+++ Classes2.cpp
@@ -201,3 +201,17 @@ int main() {
   // Close the file
   MyFile.close();
 }
+
+
+try {
+  int age = 15;
+  if (age >= 18) {
+    cout << "Access granted - you are old enough.";
+  } else {
+    throw (age);
+  }
+}
+catch (int myNum) {
+  cout << "Access denied - You must be at least 18 years old.\n";
+  cout << "Age is: " << myNum;
+}
