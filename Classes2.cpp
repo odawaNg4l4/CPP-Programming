93d70814907a10c42551c9aac50633319e17078d
odawaNg4l4 <odawangala99@gmail.com>
2023-07-24 21:42:39 +0300

    c++

--- Classes2.cpp
+++ Classes2.cpp
@@ -217,3 +217,16 @@ catch (int myNum) {
   cout << "Access denied - You must be at least 18 years old.\n";
   cout << "Age is: " << myNum;
 }
+
+try {
+  int age = 15;
+  if (age >= 18) {
+    cout << "Access granted - you are old enough.";
+  } else {
+    throw 505;
+  }
+}
+catch (int myNum) {
+  cout << "Access denied - You must be at least 18 years old.\n";
+  cout << "Error number: " << myNum;
+}
